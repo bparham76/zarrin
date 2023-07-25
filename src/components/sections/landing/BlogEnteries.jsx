6e593cfff4e80e30bc619b207c2d3876b65fc09a
Parham Baghizadeh <parham.baghizadeh@yahoo.com>
2023-07-25 18:45:23 +0330

    blog enteries mobile

--- src/components/sections/landing/BlogEnteries.jsx
+++ src/components/sections/landing/BlogEnteries.jsx
@@ -1,8 +1,10 @@
 import '../styles/blogenteries.scss';
 import { useState } from 'react';
 import { FaChevronLeft } from 'react-icons/fa6';
+import { useMobileScreen } from '../../../hooks';
 
 const BlogEnteries = () => {
+	const mobile = useMobileScreen();
 	const Entery = ({ title, summary }) => {
 		const [hover, isHover] = useState(false);
 		return (
@@ -23,17 +25,20 @@ const BlogEnteries = () => {
 		<div className='blog-container'>
 			<div className='items-container'>
 				<div className='title'>
-					<p className='heading'>آخرین مطالب وبلاگ</p>
+					<p className='heading'>
+						{mobile ? 'آخرین مقالات' : 'آخرین مطالب وبلاگ'}
+					</p>
 				</div>
 				{[...new Array(5)].map((item, index) => (
 					<Entery
 						key={index}
-						title='نر ماهی اشبل دار'
+						title='نر ماهی اشپل دار'
 						summary='لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان.'
 					/>
 				))}
 				<button>
-					مشاهده بیشتر <FaChevronLeft />
+					{mobile ? 'مشاهده همه' : 'مشاهده بیشتر'}
+					{!mobile && <FaChevronLeft />}
 				</button>
 			</div>
 		</div>
