de536a00477bbdbdbdcffed8528e6a2939afaf22
Parham Baghizadeh <parham.baghizadeh@yahoo.com>
2023-07-29 15:59:59 +0330

    billboard details mobile

--- src/components/sections/billboard-details/Gallery.jsx
+++ src/components/sections/billboard-details/Gallery.jsx
@@ -1,7 +1,36 @@
-import React from 'react';
+import './styles/gallery.scss';
+import { FcLike } from 'react-icons/fc';
+import { BsShare } from 'react-icons/bs';
+import { AiFillStar } from 'react-icons/ai';
 
 const Gallery = () => {
-	return <div>Gallery</div>;
+	return (
+		<div className='gallery-container'>
+			<div className='image'>
+				<div className='buttons'>
+					<button>
+						<FcLike />
+					</button>
+					<button>
+						<BsShare />
+					</button>
+				</div>
+			</div>
+			<div className='info'>
+				<h4>تابلو تبلیغاتی مقابل رستوران خلج در رامسر</h4>
+				<div>
+					<p>شناسه: 41556</p>
+					<p>
+						<AiFillStar className='text-yellow-700' />
+						3.5/5
+					</p>
+				</div>
+				<p>hello</p>
+				<p>hello</p>
+				<p>hello</p>
+			</div>
+		</div>
+	);
 };
 
 export default Gallery;
--- src/pages/BillboardDetails.jsx
+++ src/pages/BillboardDetails.jsx
@@ -1,12 +1,15 @@
 import './styles/billboarddetails.scss';
 import { Header, Footer } from '../components/sections';
+import { Gallery } from '../components/sections/billboard-details';
 
 const BillboardDetails = () => {
 	return (
 		<>
 			<Header />
 			<div className='page'>
-				<div className='gallery'>Gallery</div>
+				<div className='gallery'>
+					<Gallery />
+				</div>
 				<div className='cta'>Call to action</div>
 				<div className='description'>more details</div>
 			</div>
